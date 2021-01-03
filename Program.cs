e5cd4800e1290c17e3864e8fcd386adb35c8935b
TeriMHopkins <TeriMHopkins@users.noreply.github.com>
2021-01-02 16:41:38 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Student_DailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy\nStudent Daily Report\n\n");
+            Console.WriteLine("What is your name?");
+            String studentName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            String currCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            String pageNumber = Console.ReadLine();
+            int pgN = Convert.ToInt32(pageNumber);
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+            String needHelp = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics");
+            String positiveExperience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide>? Please be specific");
+            String Feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            String hrsStudy = Console.ReadLine();
+            int hours = Convert.ToInt32(hrsStudy);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly.\n\nHave a great day!");
+            
+
+        }
+    }
+}
